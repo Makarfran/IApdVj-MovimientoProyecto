fd02774853e4c7f20ebf780e2a4c930ce7661285
Daniel <daniel.belmontem@um.es>
2024-02-09 11:50:07 +0100

    cambios

---
+++ Assets/cambios/ScriptsAI/Steering/Basic/Flee.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee : SteeringBehaviour
+{
+
+    // Declara las variables que necesites para este SteeringBehaviour
+
+    
+    void Start()
+    {
+        this.nameSteering = "Flee";
+    }
+
+
+    public override Steering GetSteering(Agent agent)
+    {
+        Steering steer = new Steering();
+
+        // Calcula el steering.
+        steer.angular = 0;
+        steer.linear = agent.transform.position - target.transform.position;
+        steer.linear = steer.linear.normalized * agent.MaxAcceleration;
+
+        // Retornamos el resultado final.
+        return steer;
+    }
+}
---
+++ Assets/cambios/ScriptsAI/Steering/Basic/Seek.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : SteeringBehaviour
+{
+
+    // Declara las variables que necesites para este SteeringBehaviour
+    
+
+    
+    void Start()
+    {
+        this.nameSteering = "Seek";
+    }
+
+
+    public override Steering GetSteering(Agent agent)
+    {
+        Steering steer = new Steering();
+
+        // Calcula el steering.
+        //steer.angular = 0;
+        //steer.linear = target.transform.position - agent.transform.position;
+        //steer.linear = steer.linear.normalized * agent.MaxAcceleration;
+        
+
+        //Craig W. reynolds
+        //Vector ditancia
+        Vector3 desired_velocity = target.transform.position - agent.transform.position;
+        desired_velocity.Normalize();
+        //Aplicamos máxima aceleración
+        desired_velocity *= agent.MaxAcceleration;
+        //Calculamos el steering
+        steer.linear = desired_velocity - agent.Velocity;
+
+        // Retornamos el resultado final.
+        return steer;
+    }
+}
