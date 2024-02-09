52d1c38a642b51d220f531820a890faf2434274f
alex cajas <alexcajas202@gmail.com>
2024-02-09 12:07:36 +0100

    alex: update ignore para usar git desde la carpeta raiz del unity project

---
+++ Assets/Semana2/ScriptsAI/Steering/Basic/Seek.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : SteeringBehaviour
+{
+
+    // Declara las variables que necesites para este SteeringBehaviour
+    public Agent pers;
+
+    
+    void Start()
+    {
+        this.nameSteering = "Seek";
+    }
+
+
+    public override Steering GetSteering(Agent agent)
+    {
+        Steering steer = new Steering();
+
+        // Calcula el steering.
+        steer.angular = 0;
+        steer.linear = agent.transform.position - pers.transform.position;
+        steer.linear.Normalize();
+        steer.linear *= pers.MaxAcceleration;
+
+        // Retornamos el resultado final.
+        return steer;
+    }
+}
