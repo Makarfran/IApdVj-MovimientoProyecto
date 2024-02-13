274f357f12b581dcfbcb7bc0b079330537f22750
alex cajas <alexcajas202@gmail.com>
2024-02-13 23:28:42 +0100

    alex: Hecho y testado steering Separacion

---
+++ Assets/Semana2/ScriptsAI/Steering/Basic/Separacion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Separacion : SteeringBehaviour
+{
+
+    // Declara las variables que necesites para este SteeringBehaviour
+    public List<Agent> targets;
+    public float decayCoeficient; // k formula k/d^2
+    public float treshold;
+
+
+    
+    void Start()
+    {
+        this.nameSteering = "Separacion";
+    }
+
+
+    public override Steering GetSteering(Agent agent)
+    {
+        Steering steer = new Steering();
+
+        foreach (Agent target in targets){
+            Vector3 direction = agent.transform.position - target.transform.position;
+            float distance = direction.magnitude;
+            float aceleracion = 0;
+            if (distance < treshold) {
+                aceleracion = Mathf.Min(decayCoeficient / (distance * distance), agent.MaxAcceleration);
+            }
+
+            direction.Normalize();
+            steer.linear += aceleracion * direction; 
+        }
+
+        // Retornamos el resultado final.
+        return steer;
+    }
+}
