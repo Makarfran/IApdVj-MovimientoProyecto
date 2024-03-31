55101938ff894e95f728de3e6a784d3b4a19a7c1
Makarfran <makarfranx1@gmail.com>
2024-03-31 19:13:29 +0200

    a

--- Assets/Semana2/ScriptsAI/Grids/GridGenerator.cs
+++ Assets/Semana2/ScriptsAI/Grids/GridGenerator.cs
@@ -15,11 +15,9 @@ public class GridGenerator : MonoBehaviour {
     [SerializeField] protected List<Vector2> impasables;
     [SerializeField] protected List<Vector2>  coste2;
     [SerializeField] protected List<Vector2>  coste3;
-    [SerializeField] public Text numberText;
-    [SerializeField] public Canvas canvas;
+
     void Start(){
         posiciones = new Tile[a,b];
-        //int count = 0;
         for(int i = 0; i < a; i++ ){ 
             for(int j = 0; j < b ; j++ ){
                 Vector3 pos = new Vector3(this.transform.position.x + i*longLado, this.transform.position.y , this.transform.position.z + j*longLado  );
@@ -27,22 +25,6 @@ public class GridGenerator : MonoBehaviour {
                 spanw.name = $"Tile {i} {j}";
                 spanw.GetComponent<Tile>().setPos(pos);
                 
-                //GameObject obj = new GameObject($"Tile {i} {j}");
-               
-                                // Agregar un componente de texto al GameObject
-                
-
-                // Configurar el componente de texto 
-                /*
-                 spanw.GetComponent<Tile>().textComponent = spanw.AddComponent<Text>();
-               spanw.GetComponent<Tile>().textComponent.text = (count).ToString();
-                count++;
-                spanw.GetComponent<Tile>().textComponent.font = numberText.font;
-                spanw.GetComponent<Tile>().textComponent.fontSize = numberText.fontSize;
-                spanw.GetComponent<Tile>().textComponent.color = numberText.color;
-                spanw.GetComponent<Tile>().textComponent.transform.position = pos; 
-                spanw.GetComponent<Tile>().textComponent.transform.SetParent(canvas.transform, false);
-                */
             }
         }
     }  
