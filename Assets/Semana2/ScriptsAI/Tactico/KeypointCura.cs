2eb06dfb91dcb3b53c99ce1e38a6e2063265a2ae
Daniel <daniel.belmontem@um.es>
2024-12-05 12:33:22 +0100

    commit para pull

--- Assets/Semana2/ScriptsAI/Tactico/KeypointCura.cs
+++ Assets/Semana2/ScriptsAI/Tactico/KeypointCura.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class KeypointCura : Keypoint
 {
+    Controlador controladorJuego;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        controladorJuego = GameObject.Find("ControladorJuego").GetComponent<Controlador>();
+        Bando = "None";
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if(FindObjectOfType<InfluenceManager>()){
             int inf = getInfluenceValue(this.transform.position);
             if(inf == 1){
                 Bando = "A";
+               
             } else if (inf == -1){
                 Bando = "R";
+                
             } else {
                 Bando = "None";
             }
         }
-        
+        */
     }
 
     public int getInfluenceValue(Vector3 vec){
